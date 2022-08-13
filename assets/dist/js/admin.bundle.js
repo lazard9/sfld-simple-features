52bd648e2b4a905ca62c9dff188fb89dedc240a9
Lazar Dacic <lazar.dacic@gmail.com>
2022-08-14 00:19:10 +0200

    webpack setup

---
+++ assets/dist/js/admin.bundle.js
@@ -0,0 +1,2 @@
+!function(){"use strict";console.log("Echo 001")}();
+//# sourceMappingURL=admin.bundle.js.map
---
+++ assets/src/js/frontend.js
@@ -0,0 +1,10 @@
+import "./ajax/ajax-load-more"
+import "./ajax/ajax-voter"
+import "./swiper/swiper-initialize"
+
+// Styles
+import '../sass/frontend.scss'
+
+// Images.
+import '../icons/icons.svg'
+import '../images/the-grid.jpg'
